fa04c75656ec79318d71d8f200e212acc97c1ecb
wistful <wst.public.mail@gmail.com>
2012-10-30 14:44:09 +0300

    add errors module

---
+++ modules/errors.py
@@ -0,0 +1,30 @@
+# encoding=utf8
+
+
+class MongoOrchestrationError(Exception):
+    """Base class for all mongo-orchestration exceptions.
+    """
+
+
+class HostsError(MongoOrchestrationError):
+    """Base class for all Hosts exceptions.
+    """
+
+
+class ReplicaSetError(MongoOrchestrationError):
+    """Base class for all ReplicaSet exceptions.
+    """
+
+
+class OperationFailure(MongoOrchestrationError):
+    """Raised when an operation fails.
+    """
+
+    def __init__(self, error, code=None):
+        self.code = code
+        MongoOrchestrationError.__init__(self, error)
+
+
+class TimeoutError(OperationFailure):
+    """Raised when an operation times out.
+    """
