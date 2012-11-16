b73ac18a7eeae41f48dc773f975db17c1acfe183
wistful <wst.public.mail@gmail.com>
2012-11-16 13:44:56 +0300

    update dependencies

--- setup.py
+++ setup.py
@@ -23,13 +23,8 @@ setup(
     author='Mikhail Mamrouski',
     author_email='mmamrouski@gmail.com',
     setup_requires=[
-        'pymongo>=2.3',
+        'pymongo==2.3',
         'nose-benchmark>=0.9',
-        'psutil>=0.6.1'
-    ] + requiredPacks,
-    dependency_links=[
-        'http://github.com/mongodb/mongo-python-driver/tarball/master#egg=pymongo-2.3rc1',
-        'http://github.com/MblKiTA/nose-benchmark/tarball/master#egg=nose-benchmark-0.9',
-        'http://github.com/MblKiTA/nose-testconfig/tarball/master#egg=nose-testconfig-0.9'
-    ]
+        'psutil==0.6.1'
+    ] + requiredPacks
 )
