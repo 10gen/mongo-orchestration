5eba43439ee56de840191fa10d1c861770253b04
wistful <wst.public.mail@gmail.com>
2012-11-28 16:27:20 +0300

    set storage place from one place

---
+++ lib/__init__.py
@@ -0,0 +1,9 @@
+from hosts import Hosts
+from rs import RS
+from shards import Shards
+
+
+def set_storage(storage_path, bin_path=''):
+    Hosts().set_settings(storage_path, bin_path)
+    RS().set_settings(storage_path, bin_path)
+    Shards().set_settings(storage_path, bin_path)
