42d18a60a088f212bce9b9e7f1cc9746af381be4
wistful <wst.public.mail@gmail.com>
2012-12-21 15:53:48 +0300

    use driver 'add_user' command only if eval 'addUser' failed

--- lib/shards.py
+++ lib/shards.py
@@ -43,8 +43,10 @@ class Shard(object):
                     self.router_command(command=command, is_eval=True)
 
         if self.login:
-            pymongo.Connection(self.router['hostname']).admin.add_user(self.login, self.password)
-            # self.router_command(command="db.addUser('{login}', '{password}');".format(login=self.login, password=self.password), is_eval=True)
+            try:
+                self.router_command(command="db.addUser('{login}', '{password}');".format(login=self.login, password=self.password), is_eval=True)
+            except pymongo.errors.OperationFailure:
+                pymongo.Connection(self.router['hostname']).admin.add_user(self.login, self.password)
 
     def __init_configsvr(self, params):
         """create and start config servers"""
