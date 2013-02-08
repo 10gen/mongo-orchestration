e082cca8484832c4a7819318ced442dd51db663c
wistful <wst.public.mail@gmail.com>
2013-02-08 17:56:02 +0300

    mark line as NOQA

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distribute_setup import use_setuptools
     use_setuptools()
-    from setuptools import setup
+    from setuptools import setup  # NOQA
 
 import sys
 
