5b6af470fe94396cc7d93503a9a529cf840cc7aa
Luke Lovett <luke.lovett@mongodb.com>
2014-10-13 17:33:14 +0000

    Store all links as global constants in links.py.

--- apps/links.py
+++ apps/links.py
@@ -1,19 +1,115 @@
+#!/usr/bin/python
+# coding=utf-8
+# Copyright 2014 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Utilities for building links for discoverable API."""
+
+
+_BASE_LINKS = {
+    'get-releases': {'rel': 'get-releases', 'href': '/v1/releases',
+                     'method': 'GET'},
+    'service': {'rel': 'service', 'href': '/v1', 'method': 'GET'}
+}
+_SERVER_LINKS = {
+    'get-servers': {'method': 'GET', 'href': '{servers_href}'},
+    'add-server': {'method': 'POST', 'href': '{servers_href}'},
+    'add-server-by-id': {'method': 'PUT', 'href': '{servers_href}/{server_id}'},
+    'delete-server': {'method': 'DELETE', 'href': '{servers_href}/{server_id}'},
+    'get-server-info': {'method': 'GET', 'href': '{servers_href}/{server_id}'},
+    'server-command': {'method': 'POST', 'href': '{servers_href}/{server_id}',
+                       'template': {'action': "<action name>"},
+                       'actions': ['start', 'stop', 'restart', 'freeze']}
+}
+_REPLICA_SET_LINKS = {
+    'get-replica-set-info': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}'},
+    'get-replica-sets': {'method': 'GET', 'href': '{repls_href}'},
+    'add-replica-set': {'method': 'POST', 'href': '{repls_href}'},
+    'add-replica-set-by-id': {
+        'method': 'PUT', 'href': '{repls_href}/{repl_id}'},
+    'delete-replica-set': {
+        'href': '{repls_href}/{repl_id}', 'method': 'DELETE'},
+    'replica-set-command': {
+        'href': '{repls_href}/{repl_id}', 'method': 'POST',
+        'actions': ['reset'], 'template': {'action': '<action name>'}},
+    'get-replica-set-members': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/members'},
+    'add-replica-set-member': {
+        'method': 'POST', 'href': '{repls_href}/{repl_id}/members'},
+    'delete-replica-set-member': {
+        'method': 'DELETE',
+        'href': '{repls_href}/{repl_id}/members/{member_id}'},
+    'update-replica-set-member-config': {
+        'method': 'PATCH',
+        'href': '{repls_href}/{repl_id}/members/{member_id}'},
+    'get-replica-set-member-info': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/members/{member_id}'},
+    'get-replica-set-secondaries': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/secondaries'},
+    'get-replica-set-arbiters': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/arbiters'},
+    'get-replica-set-hidden-members': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/hidden'},
+    'get-replica-set-passive-members': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/passives'},
+    'get-replica-set-servers': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/servers'},
+    'get-replica-set-primary': {
+        'method': 'GET', 'href': '{repls_href}/{repl_id}/primary'},
+}
+_SHARDED_CLUSTER_LINKS = {
+    'add-sharded-cluster': {'method': 'POST', 'href': '{clusters_href}'},
+    'get-sharded-clusters': {'method': 'GET', 'href': '{clusters_href}'},
+    'get-sharded-cluster-info': {
+        'method': 'GET', 'href': '{clusters_href}/{cluster_id}'},
+    'sharded-cluster-command': {
+        'method': 'POST', 'href': '{clusters_href}/{cluster_id}'},
+    'add-sharded-cluster-by-id': {
+        'method': 'PUT', 'href': '{clusters_href}/{cluster_id}'},
+    'delete-sharded-cluster': {
+        'method': 'DELETE', 'href': '{clusters_href}/{cluster_id}'},
+    'add-shard': {
+        'method': 'POST', 'href': '{clusters_href}/{cluster_id}/shards'},
+    'get-shards': {
+        'method': 'GET', 'href': '{clusters_href}/{cluster_id}/shards'},
+    'get-configsvrs': {
+        'method': 'GET', 'href': '{clusters_href}/{cluster_id}/configsvrs'},
+    'get-routers': {
+        'method': 'GET', 'href': '{clusters_href}/{cluster_id}/routers'},
+    'add-router': {
+        'method': 'POST', 'href': '{clusters_href}/{cluster_id}/routers'},
+    'delete-router': {
+        'method': 'DELETE',
+        'href': '{clusters_href}/{cluster_id}/routers/{router_id}'},
+    'get-shard-info': {
+        'method': 'GET',
+        'href': '{clusters_href}/{cluster_id}/shards/{shard_id}'},
+    'delete-shard': {
+        'method': 'DELETE',
+        'href': '{clusters_href}/{cluster_id}/shards/{shard_id}'}
+}
 
 
 def base_link(rel, self_rel=False):
     """Helper for getting a link document under the API root, given a rel."""
-    links = {
-        'get-releases': {'rel': 'get-releases', 'href': '/v1/releases',
-                         'method': 'GET'},
-        'service': {'rel': 'service', 'href': '/v1', 'method': 'GET'}
-    }
-    link = links[rel]
+    link = _BASE_LINKS[rel].copy()
     link['rel'] = 'self' if self_rel else rel
     return link
 
 
 def all_base_links(rel_to=None):
+    """Get a list of all links to be included to base (/) API requests."""
     links = [
         base_link('get-releases'),
         base_link('service'),
@@ -32,24 +128,15 @@ def all_base_links(rel_to=None):
 
 def server_link(rel, server_id=None, self_rel=False):
     """Helper for getting a Server link document, given a rel."""
-    base_href = '/v1/servers'
-    server_href = '{base_href}/{server_id}'.format(**locals())
-    links = {
-        'get-servers': {'method': 'GET', 'href': base_href},
-        'add-server': {'method': 'POST', 'href': base_href},
-        'add-server-by-id': {'method': 'PUT', 'href': server_href},
-        'delete-server': {'method': 'DELETE', 'href': server_href},
-        'get-server-info': {'method': 'GET', 'href': server_href},
-        'server-command': {'method': 'POST', 'href': server_href,
-                           'template': {'action': "<action name>"},
-                           'actions': ['start', 'stop', 'restart', 'freeze']}
-    }
-    link = links[rel]
+    servers_href = '/v1/servers'
+    link = _SERVER_LINKS[rel].copy()
+    link['href'] = link['href'].format(**locals())
     link['rel'] = 'self' if self_rel else rel
     return link
 
 
 def all_server_links(server_id, rel_to=None):
+    """Get a list of all links to be included with Servers."""
     return [
         server_link(rel, server_id, self_rel=(rel == rel_to))
         for rel in ('delete-server', 'get-server-info', 'server-command')
@@ -58,46 +145,15 @@ def all_server_links(server_id, rel_to=None):
 
 def replica_set_link(rel, repl_id=None, member_id=None, self_rel=False):
     """Helper for getting a ReplicaSet link document, given a rel."""
-    base_href = '/v1/replica_sets'
-    repl_href = '{base_href}/{repl_id}'.format(**locals())
-    member_href = '{repl_href}/members/{member_id}'.format(**locals())
-    links = {
-        'get-replica-set-info': {'method': 'GET', 'href': repl_href},
-        'get-replica-sets': {'method': 'GET', 'href': base_href},
-        'add-replica-set': {'method': 'POST', 'href': base_href},
-        'delete-replica-set': {'href': repl_href, 'method': 'DELETE'},
-        'replica-set-command': {
-            'href': repl_href, 'method': 'POST', 'actions': ['reset'],
-            'template': {'action': '<action name>'}},
-        'get-replica-set-members': {
-            'method': 'GET', 'href': repl_href + '/members'},
-        'add-replica-set-member': {
-            'method': 'POST', 'href': repl_href + '/members'},
-        'delete-replica-set-member': {'method': 'DELETE', 'href': member_href},
-        'update-replica-set-member-config': {
-            'method': 'PATCH', 'href': member_href},
-        'get-replica-set-member-info': {'method': 'GET', 'href': member_href},
-        'get-replica-set-secondaries': {
-            'method': 'GET', 'href': repl_href + '/secondaries'},
-        'get-replica-set-arbiters': {
-            'method': 'GET', 'href': repl_href + '/arbiters'},
-        'get-replica-set-hidden-members': {
-            'method': 'GET', 'href': repl_href + '/hidden'},
-        'get-replica-set-passive-members': {
-            'method': 'GET', 'href': repl_href + '/passives'},
-        'get-replica-set-servers': {
-            'method': 'GET', 'href': repl_href + '/servers'},
-        'get-replica-set-primary': {
-            'method': 'GET', 'href': repl_href + '/primary'},
-    }
-    link = links[rel]
+    repls_href = '/v1/replica_sets'
+    link = _REPLICA_SET_LINKS[rel].copy()
+    link['href'] = link['href'].format(**locals())
     link['rel'] = 'self' if self_rel else rel
     return link
 
 
 def all_replica_set_links(rs_id, rel_to=None):
-    # Does not include the rel 'add-replica-set-by-id',
-    # since you can't re-add a replica set (given that the id is provided here).
+    """Get a list of all links to be included with replica sets."""
     return [
         replica_set_link(rel, rs_id, self_rel=(rel == rel_to))
         for rel in (
@@ -113,37 +169,17 @@ def all_replica_set_links(rs_id, rel_to=None):
 
 def sharded_cluster_link(rel, cluster_id=None,
                          shard_id=None, router_id=None, self_rel=False):
-    base_href = '/v1/sharded_clusters'
-    cluster_href = '{base_href}/{cluster_id}'.format(**locals())
-    router_href = '{cluster_href}/routers/{router_id}'.format(**locals())
-    shard_href = '{cluster_href}/shards/{shard_id}'.format(**locals())
-    links = {
-        'add-sharded-cluster': {'method': 'POST', 'href': base_href},
-        'get-sharded-clusters': {'method': 'GET', 'href': base_href},
-        'get-sharded-cluster-info': {'method': 'GET', 'href': cluster_href},
-        'sharded-cluster-command': {'method': 'POST', 'href': cluster_href},
-        'add-sharded-cluster-by-id': {'method': 'PUT', 'href': cluster_href},
-        'delete-sharded-cluster': {'method': 'DELETE', 'href': cluster_href},
-        'add-shard': {'method': 'POST', 'href': cluster_href + '/shards'},
-        'get-shards': {'method': 'GET', 'href': cluster_href + '/shards'},
-        'get-configsvrs': {'method': 'GET',
-                           'href': cluster_href + '/configsvrs'},
-        'get-routers': {'method': 'GET', 'href': cluster_href + '/routers'},
-        'add-router': {'method': 'POST', 'href': cluster_href + '/routers'},
-        'delete-router': {'method': 'DELETE', 'href': router_href},
-        'get-shard-info': {'method': 'GET', 'href': shard_href},
-        'delete-shard': {'method': 'DELETE', 'href': shard_href}
-    }
-    link = links[rel]
+    """Helper for getting a ShardedCluster link document, given a rel."""
+    clusters_href = '/v1/sharded_clusters'
+    link = _SHARDED_CLUSTER_LINKS[rel].copy()
+    link['href'] = link['href'].format(**locals())
     link['rel'] = 'self' if self_rel else rel
     return link
 
 
 def all_sharded_cluster_links(cluster_id, shard_id=None,
                               router_id=None, rel_to=None):
-    # Does not include the rel 'add-sharded-cluster-by-id',
-    # since you can't re-add a sharded cluster
-    # (given that the id is provided here).
+    """Get a list of all links to be included with ShardedClusters."""
     return [
         sharded_cluster_link(rel, cluster_id, shard_id, router_id,
                              self_rel=(rel == rel_to))
