ccc3b7b661c7bde2dedb5b5423ea43f851507748
Luke Lovett <luke.lovett@mongodb.com>
2014-11-05 19:53:49 +0000

    Add description and long_description to setup.

--- setup.py
+++ setup.py
@@ -16,11 +16,20 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
+extra_opts = {}
+try:
+    with open('README.rst', 'r') as fd:
+        extra_opts['long_description'] = fd.read()
+except IOError:
+    pass        # Install without README.rst
+
+
 setup(
     name='mongo-orchestration',
     version='0.1',
     author='MongoDB, Inc.',
     author_email='mongodb-user@googlegroups.com',
+    description='Restful service for managing MongoDB servers',
     install_requires=['pymongo>=2.7.2',
                       'bottle>=0.12.7',
                       'CherryPy>=3.5.0'] + extra_deps,
@@ -52,5 +61,6 @@ setup(
         'console_scripts': [
             'mongo-orchestration = mongo_orchestration.server:main'
         ]
-    }
+    },
+    **extra_opts
 )
