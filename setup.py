6ebdebb9bbe7d618e804f157ce604a0702da8b12
Luke Lovett <luke.lovett@mongodb.com>
2015-04-06 10:54:42 -0700

    BUMP 0.3.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except IOError:
 
 setup(
     name='mongo-orchestration',
-    version='0.4.dev0',
+    version='0.3.1',
     author='MongoDB, Inc.',
     author_email='mongodb-user@googlegroups.com',
     description='Restful service for managing MongoDB servers',
