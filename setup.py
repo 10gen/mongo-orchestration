7628827433830660fcfea4ff2156c3319e20534c
Luke Lovett <luke.lovett@mongodb.com>
2015-11-18 12:01:02 -0800

    BUMP 0.5

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ class test(Command):
 
 setup(
     name='mongo-orchestration',
-    version='0.5.dev0',
+    version='0.5',
     author='MongoDB, Inc.',
     author_email='mongodb-user@googlegroups.com',
     description='Restful service for managing MongoDB servers',
