d51a7457456a0dfd07d4718fbf3dedf29d793c41
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-07-30 15:14:11 -0400

    post-release bump

--- mongo_orchestration/__init__.py
+++ mongo_orchestration/__init__.py
@@ -20,7 +20,7 @@ from mongo_orchestration.servers import Servers
 from mongo_orchestration.replica_sets import ReplicaSets
 from mongo_orchestration.sharded_clusters import ShardedClusters
 
-__version__ = '0.6.4'
+__version__ = '0.6.5-dev0'
 
 
 def set_releases(releases=None, default_release=None):
