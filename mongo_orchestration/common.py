73e9a3e73e1de6730d01d6ff6674e3ad2c10d0ac
Shane Harvey <shnhrv@gmail.com>
2020-03-24 16:05:52 -0700

    Don't sepcify mechanism, instead rely on server defaults

--- mongo_orchestration/common.py
+++ mongo_orchestration/common.py
@@ -119,9 +119,10 @@ class BaseModel(object):
 
 def create_user(db, mongo_version, user, password, roles):
     if mongo_version >= (3, 7, 2):
-        # Use SCRAM_SHA-1 so that pymongo < 3.7 can authenticate.
-        db.command('createUser', user, pwd=password, roles=roles,
-                   mechanisms=['SCRAM-SHA-1', 'SCRAM-SHA-256'])
+        # Call createUser directly so that the server creates the user with
+        # both SCRAM-SHA-1 and SCRAM-SHA-256 credentials. This ensures that
+        # pymongo < 3.7 (which only supports SCRAM-SHA-1) can authenticate.
+        db.command('createUser', user, pwd=password, roles=roles)
     else:
         db.add_user(user, password=password, roles=roles)
 
