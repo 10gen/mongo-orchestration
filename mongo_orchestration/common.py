3c3e4890a1cfd7e8ba8ad795784b937ce3e23d54
Shane Harvey <shnhrv@gmail.com>
2020-03-26 14:34:29 -0700

    Ensure createUser succeeds on all replica set members (#282)

--- mongo_orchestration/common.py
+++ mongo_orchestration/common.py
@@ -122,7 +122,8 @@ def create_user(db, mongo_version, user, password, roles):
         # Call createUser directly so that the server creates the user with
         # both SCRAM-SHA-1 and SCRAM-SHA-256 credentials. This ensures that
         # pymongo < 3.7 (which only supports SCRAM-SHA-1) can authenticate.
-        db.command('createUser', user, pwd=password, roles=roles)
+        db.command('createUser', user, pwd=password, roles=roles,
+                   writeConcern=db.write_concern.document)
     else:
         db.add_user(user, password=password, roles=roles)
 
